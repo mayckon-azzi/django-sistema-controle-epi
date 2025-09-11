aa3347d3ef310a04234f2417a644034f47f34291
Jonathan Eichenberger <djohnni@hotmail.com>
2025-09-11 10:41:42 -0300

    feat: Criando sistema de mensagens personalizadas

--- app_core/views.py
+++ app_core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
     return render(request, "app_core/pages/home.html")
+
+def testar_mensagens(request):
+    messages.success(request, "Operação realizada com sucesso.")
+    messages.info(request, "Informação de exemplo.")
+    messages.warning(request, "Aviso de exemplo.")
+    messages.error(request, "Erro de exemplo.")
+    return redirect("app_core:home")
